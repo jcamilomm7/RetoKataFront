bd84af3ccb247808a3e4f616638e8106d28dc720
jcamilomm7 <88809617+jcamilomm7@users.noreply.github.com>
2022-05-07 01:30:29 -0500

    ADD --- se deja lista toda la parte funcional del reto

--- src/components/ListToList.js
+++ src/components/ListToList.js
@@ -1,6 +1,54 @@
 import React, { useState, useEffect } from "react";
-
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
+  FormGroup,
+} from "reactstrap";
 const ListToList = () => {
+  //Estados para los modales
+  const [modalEditar, setModalEditar] = useState(false);
+  const [modalEliminar, setModalEliminar] = useState(false);
+  const [tareaSeleccionada, setTareaSeleccionada] = useState({
+    id: "",
+    name: "",
+    completed: false,
+    listTodo: {
+      id: "",
+      name: "",
+    },
+  });
+
+  //Funciones para los modales
+  const seleccionarTarea = (tarea, caso) => {
+    setTareaSeleccionada(tarea);
+    caso === "Editar" ? setModalEditar(true) : setModalEliminar(true);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTareaSeleccionada((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  //Se actualiza la tarea
+  const editar = () => {
+    fetch("http://localhost:8080/api/todo", {
+      method: "PUT",
+      body: JSON.stringify(tareaSeleccionada),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((todo) => {});
+    setModalEditar(false);
+    window.location.reload();
+  };
+
   //Traer las tareas de la base de datos
   const [listaTareas, setlistaTareas] = useState([]);
   const [nombreTarea, setNombreTarea] = useState("");
@@ -56,9 +104,7 @@ const ListToList = () => {
         },
       })
         .then((response) => response.json())
-        .then((todo) => {
-          
-        });
+        .then((todo) => {});
     }
     const myformtarea = document.querySelector("#myformtarea");
     myformtarea.reset();
@@ -73,6 +119,37 @@ const ListToList = () => {
   };
   console.log(listaTareas);
   console.log(listas);
+
+  //Eliminar tarea
+  const eliminar = (id) => {
+    fetch(`http://localhost:8080/api/${id}/todo`, {
+      method: "DELETE",
+    }).then((list) => {});
+    window.location.reload();
+  };
+
+  //Actualizar tarea
+  const onChange = (event, tarea, id, nombreLista) => {
+    const request = {
+      name: tarea.name,
+      id: tarea.id,
+      completed: event.target.checked,
+      listTodo: {
+        id: id,
+        name: nombreLista,
+      },
+    };
+
+    fetch("http://localhost:8080/api/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((todo) => {});
+  };
 
   return (
     <>
@@ -112,11 +189,28 @@ const ListToList = () => {
                       <tr key={index}>
                         <td>{tarea.id}</td>
                         <td>{tarea.name}</td>
-                        <td>{tarea.completed}</td>
 
                         <td>
-                          <button>Editar</button>
-                          <button>Eliminar</button>
+                          <input
+                            type="checkbox"
+                            defaultChecked={tarea.completed}
+                            onChange={(event) =>
+                              onChange(event, tarea, id, lista.name)
+                            }
+                          ></input>
+                        </td>
+
+                        <td>
+                          <button
+                            onClick={() => seleccionarTarea(tarea, "Editar")}
+                          >
+                            Editar
+                          </button>
+                          <button
+                            onClick={() => seleccionarTarea(tarea, "Eliminar")}
+                          >
+                            Eliminar
+                          </button>
                         </td>
                       </tr>
                     );
@@ -127,6 +221,111 @@ const ListToList = () => {
           </div>
         );
       })}
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>
+          <div>
+            <h3>Editar tarea</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <FormGroup>
+            <label>ID</label>
+            <input
+              className="form-control"
+              name="id"
+              readOnly
+              type="text"
+              value={tareaSeleccionada && tareaSeleccionada.id}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label>Nombre</label>
+            <input
+              className="form-control"
+              name="name"
+              type="text"
+              value={tareaSeleccionada && tareaSeleccionada.name}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label>completada</label>
+            <input
+              className="form-control"
+              name="completada"
+              readOnly
+              type="text"
+              value={tareaSeleccionada && tareaSeleccionada.completed}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label>id lista</label>
+            <input
+              className="form-control"
+              name="id"
+              readOnly
+              type="text"
+              value={tareaSeleccionada && tareaSeleccionada.listTodo.id}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label>completada</label>
+            <input
+              className="form-control"
+              name="name"
+              readOnly
+              type="text"
+              value={tareaSeleccionada && tareaSeleccionada.listTodo.name}
+              onChange={handleChange}
+            />
+          </FormGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={editar} color="primary">
+            Editar
+          </Button>
+          <Button
+            onClick={() => {
+              setModalEditar(false);
+            }}
+            color="danger"
+          >
+            Salir
+          </Button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalEliminar}>
+        <ModalHeader>
+          <div>
+            <h3>Estas seguro de Eliminar la tarea: </h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <h2 className="tareaeliminar">{tareaSeleccionada.name}</h2>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            onClick={() => eliminar(tareaSeleccionada.id)}
+            className="btnmodaleliminar"
+          >
+            ELiminar
+          </Button>
+
+          <Button
+            className="btnmodaleliminar"
+            onClick={() => {
+              setModalEliminar(false);
+            }}
+            color="danger"
+          >
+            Salir
+          </Button>
+        </ModalFooter>
+      </Modal>
     </>
   );
 };
